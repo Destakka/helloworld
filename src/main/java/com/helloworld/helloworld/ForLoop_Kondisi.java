c5ab676dd57dc27ff0d6fad505c8023a75ffcdbd
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-08-05 09:55:58 +0700

    method perulangan dg kondisi

---
+++ src/main/java/com/helloworld/helloworld/ForLoop_Kondisi.java
@@ -0,0 +1,11 @@
+package com.helloworld.helloworld;
+
+public class ForLoop_Kondisi {
+    public static void main(String[] args) {
+        var counter = 1;
+        for (; counter <= 10; ){
+            System.out.println("Perulangan ke-" + counter);
+            counter++;
+        }
+    }
+}
