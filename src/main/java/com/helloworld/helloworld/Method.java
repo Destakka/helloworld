3908181fb78944a5038f098157f3c0478aa3ec24
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-08-05 10:17:03 +0700

    method "method"

---
+++ src/main/java/com/helloworld/helloworld/Method.java
@@ -0,0 +1,11 @@
+package com.helloworld.helloworld;
+
+public class Method {
+    public static void main(String[] args) {
+        SayHelloWorld();
+    }
+
+    static void SayHelloWorld() {
+        System.out.println("Hello World");
+    }
+}
