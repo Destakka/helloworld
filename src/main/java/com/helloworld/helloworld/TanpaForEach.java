96f78fc53d875d43d473e0339b49349a0a8469e3
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-08-05 10:11:20 +0700

    method tanpa for each

---
+++ src/main/java/com/helloworld/helloworld/TanpaForEach.java
@@ -0,0 +1,14 @@
+package com.helloworld.helloworld;
+
+public class TanpaForEach {
+    public static void main(String[] args) {
+        String[] array = {
+            "Desta", "Atika", "Nanda",
+            "TI", "Teknik", "UMY"
+        };
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+    
+}
