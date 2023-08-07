6334ddde11321fa4f4eb51cfbe7e1f33bdedf320
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-08-07 22:43:04 +0700

    Method Scope

---
+++ src/main/java/com/helloworld/helloworld/Scope.java
@@ -0,0 +1,13 @@
+package com.helloworld.helloworld;
+
+public class Scope {
+    static void sayHello(String name){
+        String hello = "Hello" + name;
+        if (!name.isBlank()) {
+            String hi = "Hi" + name;
+            System.out.println(hi);
+        }
+        System.out.println(hello);
+        System.out.println(hi);
+    }
+}
