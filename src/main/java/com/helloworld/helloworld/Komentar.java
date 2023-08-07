38237ce565c1f8f11cb334f42ecd5ec64b2d6b0d
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-08-07 22:45:28 +0700

    method komentar

---
+++ src/main/java/com/helloworld/helloworld/Komentar.java
@@ -0,0 +1,12 @@
+package com.helloworld.helloworld;
+
+public class Komentar {
+    /**
+     * @param a
+     * @param b
+     * @return
+     */
+    static int sum (int a, int b) {
+        return a + b;
+    }
+}
